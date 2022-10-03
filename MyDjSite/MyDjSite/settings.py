f3d6a041459d0991adaf810b5f422fdcf8255bdd
Yas2020 <yas.eftekhari@gmail.com>
2022-10-03 15:29:09 -0400

    more keys removed

--- MyDjSite/MyDjSite/settings.py
+++ MyDjSite/MyDjSite/settings.py
@@ -22,7 +22,7 @@ APP_NAME = 'AdsAps'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j$(&b=m(-k@_zv_)ij6-s$$u!d_r#b*7sm_y1itqqgv0yg&kkf'
+SECRET_KEY = 'Secret Key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
